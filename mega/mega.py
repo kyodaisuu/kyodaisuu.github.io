3dfe843706a0a45d090dcf88411e35473f2f18a7
Fish <37192601+kyodaisuu@users.noreply.github.com>
2022-07-14 15:03:08 +0900

    Create mega.py for calculating last digits of mega

---
+++ mega/mega.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Calculate last digit of mega
+# Author: Fish
+
+def main():
+    """Calculate last digits of mega"""
+    import sys
+    import time
+    # Set high recursion limit, the maximum depth of the Python stack.
+    # as this program uses high recursion.
+    # High recursion appears at powmod function, where the recursion level
+    # depends on the parameter rec.
+    sys.setrecursionlimit(10000)
+    # Test that this program is working properly
+    test()
+    # Calculate last digits of Mega and save to a text file
+    for d in (10, 100, 1000, 10000):  # for these digits
+        time_sta = time.time()
+        m = mega(d)
+        time_end = time.time()
+        tim = time_end - time_sta
+        filename = 'mega' + str(d) + '.txt'
+        with open(filename, mode='w') as f:
+            f.write(str(m))
+        print('Last {0} digits of Mega calculated in {1:.3}s and written to {2}.'.format(
+            d, tim, filename))
+
+
+def test():
+    """Test calculation"""
+    assert pow2mod(5, 1) == 2
+    assert pow2mod(23, 1) == 8
+    assert pow2mod(1003, 3) == 8
+    assert pow2mod(234000, 7) == 1749376
+    assert mega(1) == 6
+    assert mega(3) == 656
+    assert mega(10) == 9660742656
+
+
+def powmod(a, b, r):
+    """(a^b) mod r"""
+    if b <= 2:
+        return (a**b) % r
+    if b <= 2**100:
+        rec = 2
+    # For minimising recursion
+    elif b <= 2**10000:
+        rec = 2**100
+    else:
+        rec = 2**10000
+    q, m = divmod(b, rec)  # b = q rec + m
+    # Calculate a^b = (a^rec)^q * a^m with mod r
+    result = powmod(powmod(a, rec, r), q, r) * powmod(a, m, r)
+    return result % r
+
+
+def pow2mod(n, d):
+    """Last d digits of 2^n. Namely 2^n mod (10^d)"""
+    r = 10**d
+    if d == 1:
+        if n == 0:
+            return 1
+        # When d=1 and n>0, as 2^n mod 10 = permutation of {2, 4, 8, 6},
+        # 2^n mod 10 = 2^((n-1) mod 4 + 1) mod 10
+        return 2**((n-1) % 4 + 1) % 10
+    # Now for d > 1.
+    # From Euler's theeorem, 2^ψ(5^d) = 1 (mod 5^d)
+    # where ψ(5^d) = (5^d)(1-1/5) = 4*5^(d-1)
+    # and as 10^d is divisible by ψ(5^d) when d>1
+    # 2^(10^d) = 1 (mod 5^d) for d>1
+    # Therefore 2^(10^d + d) = 2^d (mod 10^d)
+    # It means that when n>= 10^d + d, let N = n mod 10^d and
+    # (1) when N >= d, 2^n = 2^N (mod 10^d)
+    # (2) otherwise 2^n = 2^(N + 10^d) (mod 10^d)
+    if n >= 10**d + d:
+        n = (n - d) % r + d
+    # Now we calculate (2^n) mod (10^d)
+    # We use powmod function for efficient calculation.
+    return powmod(2, n, r)
+
+
+def pow256mod(n, d):
+    """Last d digits of 256^n"""
+    # 256^n = 2^(8 * n)
+    return pow2mod(8*n, d)
+
+
+def mn(n, d):
+    """Last d digits of m(n)
+
+    where m(n) =
+      (1) 0 when n = 0
+      (2) 256^(m(n-1)) + m(n-1) otherwise
+    """
+    if n == 0:
+        return 0
+    m = mn(n-1, d)
+    m = pow256mod(m, d) + m
+    return m % (10**d)
+
+
+def mega(d):
+    """Last d digits of Mega"""
+    d = int(d)
+    if d < 1:
+        d = 1
+    m = mn(256, d)
+    m = pow256mod(m, d)
+    m = pow256mod(m, d)
+    return m
+
+
+if __name__ == '__main__':
+    main()
